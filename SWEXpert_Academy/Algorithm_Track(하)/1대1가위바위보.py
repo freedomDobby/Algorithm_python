f6eeea844d5d4faf45b51d8ff6fe594873214caf
JaeiKim <dobbykim0320@gmail.com>
2023-01-01 01:47:02 +0900

    알고리즘_Track(하) 전체 clear

--- SWEXpert_Academy/Algorithm_Track(하)/1대1가위바위보.py
+++ SWEXpert_Academy/Algorithm_Track(하)/1대1가위바위보.py
@@ -1,16 +1,21 @@
 A, B = input().split()
-A=int(A)
-B=int(B)
+A = int(A)
+B = int(B)
 
-if  A == 1 and B == 2: print("B")
+if A == 1 and B == 2:
+    print("B")
 
-if A == 1 and B == 3: print("A")
+if A == 1 and B == 3:
+    print("A")
 
-if A == 2 and B == 1: print("A")
+if A == 2 and B == 1:
+    print("A")
 
-if A == 2 and B == 3: print("B")
+if A == 2 and B == 3:
+    print("B")
 
-if A == 3 and B == 1: print("B")
+if A == 3 and B == 1:
+    print("B")
 
-if A == 3 and B ==2:  print("A")
-
+if A == 3 and B == 2:
+    print("A")

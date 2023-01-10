228b82634c3a20937fca92db40aa40754a712c1a
JaeiKim <dobbykim0320@gmail.com>
2023-01-11 00:12:37 +0900

    최댓값 오류

--- BAEKJOON/STEP/(2566)최댓값.py
+++ BAEKJOON/STEP/(2566)최댓값.py
@@ -1,3 +1,11 @@
-for i in range(9):
-    list_bag = list(map(int, input().split()))
-    print(max(list_bag))
+real_list = list(map(int, input().split()))
+answer_list = []
+
+for i in range(10):
+    for r in range(10):
+        max_value = real_list[i][r]
+        if real_list[i][r] > real_list[i][r + 1]:
+            max_value = real_list[i][r]
+
+print(max_value)
+print(i, r)
